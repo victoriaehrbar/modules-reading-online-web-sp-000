d4ef8ed2bbd7a65308a25c71f133fe1dbd45a9e4
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-10 02:52:31 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,14 +3,10 @@ require_relative './class_methods_module.rb'
 
 class Kid
   include Dance
-
+  extend MetaDancing
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
-end
-
-class Kid
-  extend MetaDancing
 end
